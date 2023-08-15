05d73a983efcfe27f92d1aeb7b23193a9eaf74cf
dparmar61 <dhavalparmar61@gmail.com>
2023-08-15 11:18:31 +0530

    updated

--- Natural_Language_Inference_production/lr_model/train.py
+++ Natural_Language_Inference_production/lr_model/train.py
@@ -1,38 +1,40 @@
+import pickle
+
+from config.core import config
+from processing.dataprocessing import get_data
+from scipy.sparse import hstack
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from processing.dataprocessing import get_data, GL_to_int
-from config.core import config
-from scipy.sparse import hstack
-import pickle
- 
-def run_training()-> None:
+
+
+def run_training() -> None:
     [corpus_train, Y_train] = get_data(config.app_config.training_data_file)
-    Y_train = GL_to_int(Y_train)
 
     corpus1_train = []
     corpus2_train = []
-    for i in range(len(corpus_train)//2):
-        corpus1_train.append(corpus_train[2*i])
-        corpus2_train.append(corpus_train[2*i+1])
+    for i in range(len(corpus_train) // 2):
+        corpus1_train.append(corpus_train[2 * i])
+        corpus2_train.append(corpus_train[2 * i + 1])
 
     vectorizer_s1 = TfidfVectorizer()
     s1_train = vectorizer_s1.fit_transform(corpus1_train)
-    f1 = open(config.app_config.sentence1_vectorizer,'wb')
-    pickle.dump(vectorizer_s1,f1)
+    f1 = open(config.app_config.sentence1_vectorizer, "wb")
+    pickle.dump(vectorizer_s1, f1)
     f1.close
 
     vectorizer_s2 = TfidfVectorizer()
     s2_train = vectorizer_s2.fit_transform(corpus2_train)
-    f2 = open(config.app_config.sentence2_vectorizer,'wb')
-    pickle.dump(vectorizer_s2,f2)
+    f2 = open(config.app_config.sentence2_vectorizer, "wb")
+    pickle.dump(vectorizer_s2, f2)
     f2.close
 
-    X_train = hstack([s1_train,s2_train])
+    X_train = hstack([s1_train, s2_train])
 
     # Classifier
-    model = LogisticRegression(max_iter = config.model_config.max_iterations)
+    model = LogisticRegression(max_iter=config.model_info_config.max_iterations)
     model.fit(X_train, Y_train)
-    pickle.dump(model,open(config.model_config.model_path,'wb'))
-        
-if __name__=="__main__":
+    pickle.dump(model, open(config.model_info_config.output_model_path, "wb"))
+
+
+if __name__ == "__main__":
     run_training()
